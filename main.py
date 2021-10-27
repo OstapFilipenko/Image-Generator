44b473d30143d320109e9f1e041146d1ffd15705
OstapFilipenko <info@ostapfilipenko.com>
2021-10-27 17:00:03 +0200

    created the change color function

--- main.py
+++ main.py
@@ -4,22 +4,25 @@ from PIL import Image
 old_color = 255, 112, 171, 255
 new_color = 66, 135, 245, 255
 
-for path in glob.glob("assets/FK-Square.png"):
-    if "__out" in path:
-        print("skipping on: " + path)
-        continue
+def change_color(file_path, o_color, n_color):
+    for path in glob.glob(file_path):
+        if "__out" in path:
+            print("skipping on: " + path)
+            continue
 
-    print ("working on: " + path)
+        print ("working on: " + path)
 
-    im = Image.open(path)
-    im = im.convert("RGBA")
-    width, height = im.size
-    colortuples = im.getcolors()
+        im = Image.open(path)
+        im = im.convert("RGBA")
+        width, height = im.size
+        colortuples = im.getcolors()
 
-    pix = im.load()
-    for x in range(0, width):
-        for y in range(0, height):
-            if pix[x,y] == old_color:
-                im.putpixel((x, y), new_color)
+        pix = im.load()
+        for x in range(0, width):
+            for y in range(0, height):
+                if pix[x,y] == o_color:
+                    im.putpixel((x, y), n_color)
 
-    im.save(path+"__out.png")
+        return im
+
+change_color("assets/FK-Square.png", old_color, new_color).save("lol.png")
